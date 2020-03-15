cc13c40ce9caaabb489db34c6422e49805af348b
TeOdodin <34354482+TeOdodin@users.noreply.github.com>
2020-03-15 17:38:24 +0100

    Avancement Design

---
+++ project/index.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="contents/css/global.css">
+        <link rel="stylesheet" type="text/css" href="contents/css/header.css">
+        <link rel="stylesheet" type="text/css" href="contents/css/article.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a>
+                    Accueil
+                </a>
+                <a>
+                    Points de collecte
+                </a>
+                <a>
+                    Sources
+                </a>
+            </nav>
+            <h1>
+                EcoNantes
+            </h1>
+        </header>
+        <section>
+            <article>
+                <h3>Présentation du site</h3>
+                <div>
+                    <p>
+                    </p>
+                    <img />
+                </div>
+            </article>
+            <article>
+                <h3></h3>
+                <div>
+                    <p>
+                    </p>
+                    <img />
+                </div>
+            </article>
+        </section>
+    </body>
+</html>
